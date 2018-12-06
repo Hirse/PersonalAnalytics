2abc3c1382ea04a07c261c0dca6a53809bbd6cc3
Jan Pilzer <jan.pilzer@gmx.de>
2018-12-06 15:53:40 -0800

    Fix MissingGazeTimeout and add comments

--- src/client/AM.PA.MonitoringTool/TobiiTracker/FixationWindowConsumer.cs
+++ src/client/AM.PA.MonitoringTool/TobiiTracker/FixationWindowConsumer.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Timers;
 using TobiiTracker.Data;
 using TobiiTracker.Helpers;
 
@@ -13,8 +12,6 @@ namespace TobiiTracker
 
         private readonly Dictionary<IntPtr, FixationWindowEntry> _lastFixationOfWindow;
         private readonly HighlighterOverlay _overlay;
-        private readonly Timer _missingGazeTimer;
-
         private FixationWindowEntry _lastFixation;
 
         public bool Stopped { get; private set; } = true;
@@ -22,29 +19,34 @@ namespace TobiiTracker
         internal FixationWindowConsumer(HighlighterOverlay overlay)
         {
             _lastFixationOfWindow = new Dictionary<IntPtr, FixationWindowEntry>();
-            _missingGazeTimer = new Timer(MissingGazeTimeout)
-            {
-                AutoReset = false
-            };
-            _missingGazeTimer.Elapsed += MissingGazeTimerOnElapsed;
             _overlay = overlay;
         }
 
         internal void Collect(object sender, FixationWindowEntry currentFixation)
         {
+            // Consumer stopped: Ignore all events
             if (Stopped) return;
 
-            _missingGazeTimer.Stop();
-            _missingGazeTimer.Start();
-
+            // Fixated Window is blacklisted - Ignore fixation
             if (WindowUtils.IsBlacklisted(currentFixation.ProcessName, currentFixation.WindowTitle)) return;
 
+            // Fixation on the same window as the last
             if (currentFixation.WindowHandle == _lastFixation.WindowHandle)
             {
+                // Hide overlay if still visible and fixation in highlight area
                 _overlay.HideConditionally(currentFixation.X, currentFixation.Y);
+
+                // If the last gaze on the same window was long ago, highlight
+                var timeDiff = currentFixation.Time - _lastFixation.Time;
+                if (timeDiff.TotalMilliseconds > MissingGazeTimeout)
+                {
+                    Visualize(_lastFixationOfWindow[currentFixation.WindowHandle]);
+                }
             }
+            // Fixation on different window
             else if (_lastFixationOfWindow.ContainsKey(currentFixation.WindowHandle))
             {
+                // Only highlight if the last fixation on the current window is not too recent
                 var timeDiff = currentFixation.Time - _lastFixationOfWindow[currentFixation.WindowHandle].Time;
                 if (timeDiff.TotalMilliseconds > VisualizationThreshold)
                 {
@@ -65,18 +67,12 @@ namespace TobiiTracker
         public void Stop()
         {
             _overlay.Stop();
-            _missingGazeTimer.Stop();
             Stopped = true;
         }
 
         private void Visualize(FixationWindowEntry fixationWindowEntry)
         {
             _overlay.Show(fixationWindowEntry.X, fixationWindowEntry.Y);
-        }
-
-        private void MissingGazeTimerOnElapsed(object sender, ElapsedEventArgs e)
-        {
-            Visualize(_lastFixationOfWindow[_lastFixation.WindowHandle]);
         }
     }
 }
